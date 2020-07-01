833a8ea0f2e37bd6a324a48939b560f16874139b
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-07-01 11:47:46 +0300

    add player

---
+++ Smart Tools/Assets/Scripts/PlayerModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class PlayerModel
+{
+    public Vector2ReactiveProperty WorldPosition = new Vector2ReactiveProperty();
+
+    public Vector2Int WorldToQuadPosition()
+    {
+        int quadX = Mathf.RoundToInt(WorldPosition.Value.x / 2);//get rid of 2
+        int quadY = Mathf.RoundToInt(WorldPosition.Value.y / 2);
+        return new Vector2Int(quadX, quadY);
+    }
+
+}
---
+++ Smart Tools/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _speed = 10f;
+
+    private bool _isGameStarted = false;
+
+    private PlayerModel _playerModel;
+    private Vector3 _movementDirection;
+
+    [Inject]
+    private void Construct(PlayerModel playerModel)
+    {
+        _playerModel = playerModel;
+    }
+
+    private void Awake()
+    {
+        _movementDirection = Vector3.forward;      
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown && !_isGameStarted)
+        {
+            _isGameStarted = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.A) && _isGameStarted)
+        {
+            _movementDirection = Vector3.forward;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && _isGameStarted)
+        {
+            _movementDirection = Vector3.right;
+        }
+
+        if (_isGameStarted)
+        {
+            transform.localPosition += _movementDirection * Time.deltaTime * _speed;
+            _playerModel.WorldPosition.Value = new Vector2(transform.localPosition.x, transform.localPosition.z);
+        }
+    }
+}
